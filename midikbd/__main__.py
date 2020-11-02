a8df8e44213dc5bcda0689319c532397936f4d60
nonk123 <Sergeyii321@gmail.com>
2020-11-02 19:42:29 +0300

    Rework keyboard controls
    The third argument is the "root" note, the lowest one available. On
    en_US keyboard you would play it by pressing "Q".
    
    The keyboard layout was rearranged. Instead of keycode = MIDI note, the
    letter keys (and punctuation) now correspond to a number _added_ to the
    root note. Since "Q" plays the lowest note, its number is just 0. "A"
    plays a note one octave higher, so its number is 12. "]" plays the note
    before that, so 11; etc.
    
    Forking leaves MIDI ports behind (why?), so it was removed.

--- midikbd/__main__.py
+++ midikbd/__main__.py
@@ -1,5 +1,4 @@
 import sys
-import os
 
 from Xlib.display import Display
 from Xlib.ext import xinput, ge
@@ -7,76 +6,101 @@ from Xlib import X
 
 import mido
 
+def midi_note_to_string(midi):
+    """Convert MIDI note number to music note string. E.g. 28 -> E1"""
+    names = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+    # I have absolutely no idea how this works, but it does.
+    return names[midi % 12] + str((midi - 12) // 12)
+
 def print_usage():
     print("Usage:")
-    print("  midikbd <device_id> <port_name>\n")
+    print("  midikbd <device_id> <port_name> <root_note>\n")
     print("Where:")
     print("  device_id - keyboard device ID from `xinput`")
     print("  port_name - the name of the virtual port to be created")
+    print("  root_note - MIDI note number the lowest note is assigned")
+
+def parse_args():
+    try:
+        assert len(sys.argv) == 4
+        return (int(sys.argv[1]), sys.argv[2], int(sys.argv[3]))
+    except Exception as err:
+        print("Error ", type(err), ": ", err, "\n", sep="")
+        print_usage()
+        sys.exit(1)
 
 def main():
+    device_id, port_name, root_note = parse_args()
+
+    print("Root note:", midi_note_to_string(root_note))
+
+    held_keys = {}
+
+    def is_held(keycode):
+        return keycode in held_keys and held_keys[keycode]
+
     display = Display()
 
-    try:
-        assert len(sys.argv) == 3
-
-        device_id = int(sys.argv[1])
-
+    def establish_grab():
         root = display.screen().root
+        time = X.CurrentTime
+        async_ = xinput.GrabModeAsync
+        mask = xinput.KeyPressMask | xinput.KeyReleaseMask
 
-        root.change_attributes(event_mask = X.KeyPress | X.KeyRelease)
-        root.xinput_grab_device(device_id, X.CurrentTime,
-                                xinput.GrabModeAsync, xinput.GrabModeAsync, True,
-                                xinput.KeyPressMask | xinput.KeyReleaseMask)
+        root.xinput_grab_device(device_id, time, async_, async_, True, mask)
 
+    midi_port = mido.open_output(sys.argv[2], virtual=True)
+    print(f'Output "{midi_port.name}" open')
+
+    with midi_port:
+        establish_grab()
         print("Grabbed device", device_id)
+        print("Press ^C to exit (on grabbed device)")
 
-        held_keys = {}
+        while True:
+            event = display.next_event()
 
-        def is_held(keycode):
-            return keycode in held_keys and held_keys[keycode]
+            # XInput sends generic events only.
+            if event.type != ge.GenericEventCode:
+                continue
 
-        with mido.open_output(sys.argv[2], virtual=True) as midi_port:
-            print(f'Output "{midi_port.name}" open')
-            print("Press ^C to exit (works globally)")
+            keycode = event.data.detail
 
-            new_pid = os.fork()
+            # ^C detection.
+            if (is_held(37) or is_held(109)) and keycode == 54:
+                break
 
-            if new_pid != 0:
-                return
+            # Hold the note until the key is released.
+            if event.evtype == xinput.KeyPress and not is_held(keycode):
+                message = mido.Message("note_on")
+                held_keys[keycode] = True
+            elif event.evtype == xinput.KeyRelease:
+                message = mido.Message("note_off")
+                held_keys[keycode] = False
+            else:
+                continue
 
-            while True:
-                event = display.next_event()
+            # Most likely not portable, but that's not an issue yet.
+            # On en_US layout, these are the keys Q-], A-\, and Z-/
+            # The ranges are inclusive, hence the "+1".
 
-                if event.type != ge.GenericEventCode:
-                    continue
+            if keycode in range(24, 35 + 1):
+                # Difference from root note.
+                # Q = root note, so 24 - 24 = 0.
+                note = keycode - 24
+            elif keycode in range(38, 48 + 1):
+                # Compensate for 35-38 transition.
+                note = keycode - 24 - 2
+            elif keycode in range(51, 61 + 1):
+                # 35-38 and 48-51 transition.
+                note = keycode - 24 - 2 - 2
+            else:
+                continue
 
-                keycode = event.data.detail
+            message.note = note + root_note
+            message.velocity = 127 # too quiet otherwise
 
-                # ^C detection.
-                if (is_held(37) or is_held(109)) and keycode == 54:
-                    break
-
-                # Hold the note until the key is released.
-                if event.evtype == xinput.KeyPress and not is_held(keycode):
-                    message = mido.Message("note_on")
-                    held_keys[keycode] = True
-                elif event.evtype == xinput.KeyRelease:
-                    message = mido.Message("note_off")
-                    held_keys[keycode] = False
-                else:
-                    continue
-
-                # Ignore notes out of range.
-                if keycode >= 0 and keycode <= 127:
-                    message.note = keycode
-                    midi_port.send(message)
-    except Exception as err:
-        print("Error: ", err, "\n", sep="")
-        print_usage()
-        exit(1)
-    finally:
-        display.close()
+            midi_port.send(message)
 
 if __name__ == "__main__":
     main()
